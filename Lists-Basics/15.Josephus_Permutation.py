898418a8150270407cbd48096b834dc5ac97cb24
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-10-28 20:15:05 +0100

    Added New Task

---
+++ Lists-Basics/15.Josephus_Permutation.py
@@ -0,0 +1,16 @@
+numbers = input().split(' ')
+killed = int(input())
+output = []
+counter = 0
+current_index = 0
+
+while len(numbers) > 0:
+    counter += 1
+    if counter % killed == 0:
+        output.append(numbers[current_index])
+        numbers.pop(current_index)
+    else:
+        current_index += 1
+    if current_index >= len(numbers):
+        current_index = 0
+print(str(output).replace(' ', '').replace('\'', ''))
