2bc9f7770fa911ff7125c69d7863d9a933674fd6
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:36:10 +0000

    Decipher This

--- 06.Lists Advanced/15.Decipher This.py
+++ 06.Lists Advanced/15.Decipher This.py
@@ -3,18 +3,21 @@ starting_index = 0
 number = ''
 rest_of_the_string = ''
 message = ''
+
 for iteration in range(len(secret_message)):
     word = secret_message[starting_index]
+    
     for num in word:
         if num.isdigit():
             number += num
         else:
             rest_of_the_string += num
     mid = rest_of_the_string[1:len(rest_of_the_string) - 1]
+    
     if len(rest_of_the_string) < 2:
         swapped_letters = rest_of_the_string[len(rest_of_the_string) - 1] + mid
     else:
-        swapped_letters = rest_of_the_string[len(rest_of_the_string) - 1] + mid + rest_of_the_string[0]
+        swapped_letters = rest_of_the_string[len(rest_of_the_string) - 1] + mid + rest_of_the_string[0]        
     message += chr(int(number))
     message += swapped_letters
     starting_index += 1
