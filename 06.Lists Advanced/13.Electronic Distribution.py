968d602f0433f3ffba5bf35c4575ee7c50dbd4ed
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:34:30 +0000

    Electronic Distribution

--- 06.Lists Advanced/13.Electronic Distribution.py
+++ 06.Lists Advanced/13.Electronic Distribution.py
@@ -5,12 +5,14 @@ filled_shells = []
 
 for electron in range(1, number_of_electrons + 1):
     maximum_number = 2 * electron ** 2
+    
     if copy_number_of_electrons < maximum_number:
         filled_shells.append(copy_number_of_electrons)
         break
     else:
         filled_shells.append(maximum_number)
     copy_number_of_electrons -= maximum_number
+    
     if copy_number_of_electrons == 0:
         break
 print(filled_shells)
