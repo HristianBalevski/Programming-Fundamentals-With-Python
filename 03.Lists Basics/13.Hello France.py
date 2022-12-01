84da48085114b4953261bad842cb806643acc641
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:21:04 +0000

    Hello France

--- 03.Lists Basics/13.Hello France.py
+++ 03.Lists Basics/13.Hello France.py
@@ -15,21 +15,25 @@ for item in collection_of_items:
         new_price = float(price_of_product) + float(price_of_product) * 0.40
         bought_items.append(new_price)
         profit += new_price - float(price_of_product)
+        
     elif type_of_product == 'Shoes' and float(price_of_product) <= 35:
         budget -= float(price_of_product)
         new_price = float(price_of_product) + float(price_of_product) * 0.40
         bought_items.append(new_price)
         profit += new_price - float(price_of_product)
+        
     elif type_of_product == 'Accessories' and float(price_of_product) <= 20.50:
         budget -= float(price_of_product)
         new_price = float(price_of_product) + float(price_of_product) * 0.40
         bought_items.append(new_price)
         profit += new_price - float(price_of_product)
+        
 all_money = sum(bought_items) + budget
 for number in bought_items:
     print(f'{number:.2f}', end=" ")
 print()
 print(f"Profit: {profit:.2f}")
+
 if all_money >= 150:
     print("Hello, France!")
 else:
