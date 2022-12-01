c2b3589c5d4d95a90f377f3ddd7cf7fee665322f
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:34:00 +0000

    Office Chairs

--- 06.Lists Advanced/12.Office Chairs.py
+++ 06.Lists Advanced/12.Office Chairs.py
@@ -8,8 +8,10 @@ for room in range(1, number_of_rooms + 1):
     additional_information = input().split(' ')
     chairs = additional_information[0]
     visitors = additional_information[1]
+    
     if len(chairs) > int(visitors):
         chairs_left += len(chairs) - int(visitors)
+        
     elif len(chairs) < int(visitors):
         needed_chairs += abs(len(chairs) - int(visitors))
         print(f'{needed_chairs} more chairs needed in room {room}')
