ca2257e7de07791865f02310badbea75ca27624d
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:47:06 +0000

    Replace Repeating Chars

--- 09.Text Processing/11.Replace Repeating Chars.py
+++ 09.Text Processing/11.Replace Repeating Chars.py
@@ -6,6 +6,7 @@ for index in range(len(given_text) - 1):
     if given_text[index] != given_text[index + 1]:
         replaced_text += given_text[index]
         last_letter = given_text[index]
+        
 if given_text[-1] != last_letter:
     replaced_text += given_text[-1]
     print(replaced_text)
