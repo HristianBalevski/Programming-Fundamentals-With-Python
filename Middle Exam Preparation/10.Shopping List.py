7a21132e1b090d6fd0561f26c5ce929b304c1635
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 21:50:10 +0000

    Shopping List

--- Middle Exam Preparation/10.Shopping List.py
+++ Middle Exam Preparation/10.Shopping List.py
@@ -12,14 +12,17 @@ while True:
     if current_command == 'Urgent':
         if product not in initial_list:
             initial_list.insert(0, product)
+            
     elif current_command == 'Unnecessary':
         if product in initial_list:
             initial_list.remove(product)
+            
     elif current_command == 'Correct':
         if product in initial_list:
             new_product = data[2]
             old_product_index = initial_list.index(product)
             initial_list[old_product_index] = new_product
+            
     elif current_command == 'Rearrange':
         if product in initial_list:
             initial_list.remove(product)
