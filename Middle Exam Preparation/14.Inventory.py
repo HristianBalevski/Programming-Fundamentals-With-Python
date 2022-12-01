9e3b1493d69496eda55ef9b81b898b687849f40f
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 21:53:32 +0000

    Inventory

--- Middle Exam Preparation/14.Inventory.py
+++ Middle Exam Preparation/14.Inventory.py
@@ -8,10 +8,12 @@ while data != 'Craft!':
         item = command[1]
         if item not in journal:
             journal.append(item)
+            
     elif command[0] == 'Drop':
         point = command[1]
         if point in journal:
             journal.remove(point)
+            
     elif command[0] == 'Combine Items':
         separate = command[1].split(':')
         old_item = separate[0]
@@ -19,6 +21,7 @@ while data != 'Craft!':
         if old_item in journal:
             index_old_item = journal.index(old_item)
             journal.insert(index_old_item + 1, new_item)
+            
     elif command[0] == 'Renew':
         article = command[1]
         if article in journal:
