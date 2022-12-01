95cf88f3821453cbb0ea7fbce6929be8719fbea3
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:37:35 +0000

    Odd Occurrences

--- 08.Dictionaries/06.Odd Occurrences.py
+++ 08.Dictionaries/06.Odd Occurrences.py
@@ -7,6 +7,7 @@ for element in words:
 
 for index in range(len(words_lower)):
     searched_word = words_lower[index]
+    
     if words_lower.count(searched_word) % 2 != 0:
         if searched_word not in already_printed:
             print(searched_word, end=" ")
