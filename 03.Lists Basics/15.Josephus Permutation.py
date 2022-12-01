a45a84559f23863dc66a32c02fc512c652538ddf
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:21:59 +0000

    Josephus Permutation

--- 03.Lists Basics/15.Josephus Permutation.py
+++ 03.Lists Basics/15.Josephus Permutation.py
@@ -6,11 +6,13 @@ current_index = 0
 
 while len(numbers) > 0:
     counter += 1
+    
     if counter % killed == 0:
         output.append(numbers[current_index])
         numbers.pop(current_index)
     else:
         current_index += 1
+        
     if current_index >= len(numbers):
         current_index = 0
 print(str(output).replace(' ', '').replace('\'', ''))
