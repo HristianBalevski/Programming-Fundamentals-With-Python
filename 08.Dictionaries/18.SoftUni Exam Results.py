97b5def93f0308262320753c9bdf850d40d4fba9
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:42:12 +0000

    SoftUni Exam Results

--- 08.Dictionaries/18.SoftUni Exam Results.py
+++ 08.Dictionaries/18.SoftUni Exam Results.py
@@ -5,11 +5,13 @@ while command != 'exam finished':
     data = command.split('-')
     student_name = data[0]
     program_language = data[1]
+    
     if program_language not in submissions and program_language != 'banned':
         submissions[program_language] = 1
     else:
         if program_language != 'banned':
             submissions[program_language] += 1
+            
     if program_language == 'banned':
         del exam_results[student_name]
         command = input()
@@ -19,9 +21,11 @@ while command != 'exam finished':
     if student_name not in exam_results:
         exam_results[student_name] = []
         exam_results[student_name] = [program_language, points]
+        
     elif student_name in exam_results and program_language in exam_results[student_name]:
         if exam_results[student_name][1] < points:
             exam_results[student_name][1] = points
+            
     elif student_name in exam_results and program_language not in exam_results[student_name]:
         exam_results[student_name] += [program_language, points]
     command = input()
@@ -30,5 +34,6 @@ print('Results:')
 for username, point in exam_results.items():
     print(f'{username} | {point[1]}')
 print('Submissions:')
+
 for languages, submissions_count in submissions.items():
     print(f'{languages} - {submissions_count}')
