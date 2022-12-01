69665e2b5f096e7d777fc7a27aa708b5d40f0042
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-01 22:47:32 +0000

    String Explosion

--- 09.Text Processing/12.String Explosion.py
+++ 09.Text Processing/12.String Explosion.py
@@ -5,6 +5,7 @@ new_text = ''
 for index in range(len(data)):
     if strength > 0 and data[index] != '>':
         strength -= 1
+        
     elif data[index] == '>':
         new_text += data[index]
         strength += int(data[index + 1])
