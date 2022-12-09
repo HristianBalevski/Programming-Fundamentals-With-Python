e75ca1ce42ef549a99a93f6d47856120da2f977a
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-09 20:14:51 +0000

    Company Users Update

--- 08.Dictionaries/17.Company Users.py
+++ 08.Dictionaries/17.Company Users.py
@@ -1,18 +1,20 @@
-company_users = {}
-command = input()
+data = input()
+my_dictionary = {}
 
-while command != 'End':
-    information = command.split(' -> ')
-    company_name = information[0]
-    employee_id = information[1]
+while data != 'End':
+    data = data.split(' -> ')
 
-    if company_name not in company_users:
-        company_users[company_name] = []
-    if employee_id not in company_users[company_name]:
-        company_users[company_name].append(employee_id)
-    command = input()
+    company_name = data[0]
+    employee_id = data[1]
 
-for company, employee in company_users.items():
-    print(company)
-    final_data = '\n-- '.join(employee)
-    print(f"-- {final_data}")
+    if company_name not in my_dictionary:
+        my_dictionary[company_name] = [employee_id]
+    else:
+        if employee_id not in my_dictionary[company_name]:
+            my_dictionary[company_name].append(employee_id)
+    data = input()
+
+for key, value in my_dictionary.items():
+    print(key)
+    for index in range(len(value)):
+        print(f'-- {value[index]}')
