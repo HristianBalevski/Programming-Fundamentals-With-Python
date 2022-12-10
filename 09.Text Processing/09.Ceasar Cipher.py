5f9993cdeb829b5db7764d63734c21a21600c0b5
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-10 10:10:28 +0000

    Ceasar Cipher Update

--- 09.Text Processing/09.Ceasar Cipher.py
+++ 09.Text Processing/09.Ceasar Cipher.py
@@ -5,3 +5,10 @@ for char in data:
     symbol = ord(char) + 3
     cipher += chr(symbol)
 print(cipher)
+
+# The same task solved with list comprehension.
+
+text = input()
+
+encrypted_text = "".join([chr(ord(char) + 3) for char in text])
+print(encrypted_text)
