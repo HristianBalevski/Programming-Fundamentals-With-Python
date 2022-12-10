4982a0c479517e3f7e8151338718e994b22fd490
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2022-12-10 12:07:21 +0000

    Replace Repeating

--- 09.Text Processing/11.Replace Repeating Chars.py
+++ 09.Text Processing/11.Replace Repeating Chars.py
@@ -1,14 +1,20 @@
-given_text = input()
-replaced_text = ''
-last_letter = ''
+text = input()
+new_text = ''
+unique_counter = 0
 
-for index in range(len(given_text) - 1):
-    if given_text[index] != given_text[index + 1]:
-        replaced_text += given_text[index]
-        last_letter = given_text[index]
+for index in range(len(text) - 1):
+    if text[index] != text[index + 1] and unique_counter == 0:
+        new_text += text[index]
+
+    if text[index] == text[index + 1]:
+        if unique_counter == 0:
+            new_text += text[index]
+            unique_counter += 1
+    else:
+        unique_counter = 0
         
-if given_text[-1] != last_letter:
-    replaced_text += given_text[-1]
-    print(replaced_text)
+if text[-1] != text[-2]:
+    new_text += text[-1]
+    print(new_text)
 else:
-    print(replaced_text)
+    print(new_text)
