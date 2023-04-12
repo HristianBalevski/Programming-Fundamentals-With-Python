97812bb0db14aec52eb4afc9d2c0845df37a91fa
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-04-12 10:02:28 +0100

    Homework Lab

--- 05.Functions/12.Even Numbers.py
+++ 05.Functions/12.Even Numbers.py
@@ -1,6 +1,3 @@
-sequence_of_numbers = input().split()
-even_nums = []
-for num in sequence_of_numbers:
-    even_nums.append(int(num))
-result = filter(lambda x: x % 2 == 0, even_nums)
+numbers = [int(x) for x in input().split()]
+result = filter(lambda x: x % 2 == 0, numbers)
 print(list(result))
